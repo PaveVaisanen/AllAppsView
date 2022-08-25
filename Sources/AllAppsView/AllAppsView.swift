b4cf4dab71e36bd2b87a0b2e24c07f149fd58360
Pave <pavevaisanen@me.com>
2022-08-25 15:46:32 +0300

    No image displayd

--- Sources/AllAppsView/AllAppsView.swift
+++ Sources/AllAppsView/AllAppsView.swift
@@ -148,15 +148,15 @@ struct MyAppView: View {
                         axis: (x: 1, y: 0, z: 0)
                     )
                 
-                Image(packageResource: app.imageName, ofType: "png")
-                    .resizable()
-                    .frame(width: 80, height: 80)
-                    .shadow(
-                        color:.black.opacity(0.2),
-                        radius: 4,
-                        x: orientationCorrectedX * 20,
-                        y: orientationCorrectedY * 20
-                    )
+//                Image(packageResource: app.imageName, ofType: "png")
+//                    .resizable()
+//                    .frame(width: 80, height: 80)
+//                    .shadow(
+//                        color:.black.opacity(0.2),
+//                        radius: 4,
+//                        x: orientationCorrectedX * 20,
+//                        y: orientationCorrectedY * 20
+//                    )
                 
             }
             
@@ -193,28 +193,28 @@ struct MyAppView: View {
 //}
 
 
-@available(iOS 14, macOS 11.0, *)
-extension Image {
-    init(packageResource name: String, ofType type: String) {
-#if canImport(UIKit)
-        guard let path = Bundle.module.path(forResource: name, ofType: type),
-              let image = UIImage(contentsOfFile: path) else {
-            self.init(name)
-            return
-        }
-        self.init(uiImage: image)
-#elseif canImport(AppKit)
-        guard let path = Bundle.module.path(forResource: name, ofType: type),
-              let image = NSImage(contentsOfFile: path) else {
-            self.init(name)
-            return
-        }
-        self.init(nsImage: image)
-#else
-        self.init(name)
-#endif
-    }
-}
+//@available(iOS 14, macOS 11.0, *)
+//extension Image {
+//    init(packageResource name: String, ofType type: String) {
+//#if canImport(UIKit)
+//        guard let path = Bundle.module.path(forResource: name, ofType: type),
+//              let image = UIImage(contentsOfFile: path) else {
+//            self.init(name)
+//            return
+//        }
+//        self.init(uiImage: image)
+//#elseif canImport(AppKit)
+//        guard let path = Bundle.module.path(forResource: name, ofType: type),
+//              let image = NSImage(contentsOfFile: path) else {
+//            self.init(name)
+//            return
+//        }
+//        self.init(nsImage: image)
+//#else
+//        self.init(name)
+//#endif
+//    }
+//}
 
 
 
